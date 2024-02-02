d863ef7ddddbcea526346659af04250ce59687e9
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2024-02-01 22:40:18 -0800

    working on nav

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../styles/LandingPage.css';
-// import Nav from '../components/Nav.jsx'
+import Nav from '../components/Nav.jsx'
 
 
 const LandingPage = () => {
   return (
     <div className="container">
-      {/* <Nav /> */}
+      <Nav />
       <h1 className="header">Welcome to Our Special Day</h1>
       <p className="subtext">Join us in celebrating love and friendship.</p>
       <button className="ctaButton" onClick={() => alert('Thank you for your interest!')}>
